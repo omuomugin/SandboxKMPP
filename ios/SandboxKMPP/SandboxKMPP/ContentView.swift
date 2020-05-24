3e8b9e01359676fb61e504488e91c2e3ec8bd998
omuomugin <omuomugin@out-box.dev>
2020-05-24 12:00:00 +0900

    feat: add ios app

---
+++ ios/SandboxKMPP/SandboxKMPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SandboxKMPP
+//
+//  Created by omuomugin on 2020/05/24.
+//  Copyright © 2020 omuomugin. All rights reserved.
+//
+
+import SwiftUI
+import SharedCode
+
+struct ContentView: View {
+    let message = CommonKt.createApplicationScreenMessage()
+    
+    var body: some View {
+        Text(message)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
